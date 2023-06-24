58baa144003ce42baa71949b79b0de8d0d25a5c5
Kavya Kapadia <kavyakapadia@outlook.com>
2023-06-24 13:00:02 +0100

    Fixed no Key error for child

--- components/home/upcomingmatches/UpcomingMatches.jsx
+++ components/home/upcomingmatches/UpcomingMatches.jsx
@@ -45,6 +45,7 @@ function UpcomingMatches(props) {
               item={item}
               selectedMatch={selectedMatch}
               handleCardPress={handleCardPress}
+              key={(item) => item} // TODO: Temp key, add key from API when needed
             />
           ))
         )}
