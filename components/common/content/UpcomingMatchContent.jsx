9495a3f62fce10a4e3000581d7d5ed94dc78d27e
Kavya Kapadia <kavyakapadia@outlook.com>
2023-06-25 14:13:43 +0100

    Fixed previous error- switch, current render error

---
+++ components/common/content/UpcomingMatchContent.jsx
@@ -0,0 +1,42 @@
+import { View, Text, ActivityIndicator } from "react-native";
+import React, { useState } from "react";
+import UpcomingMatchesCard from "../cards/UpcomingMatchesCard";
+import useFetch from "../../../hook/useFetch";
+
+const UpcomingMatchContent = (dateToPassAsQueryItem) => {
+  const [selectedMatch, setSelectedMatch] = useState(null);
+  const handleCardPress = (id) => {
+    // TODO: Route to a specific live match
+    setSelectedMatch(id);
+  };
+  //   const { data, isLoading, error } = useFetch(
+  //     `sports/1/events/date/${dateToPassAsQuery}`,
+  //     {
+  //       page: "1",
+  //     }
+  //   );
+  console.log(dateToPassAsQueryItem);
+  return (
+    <View>
+      {/* {isLoading ? (
+        <ActivityIndicator size="large" colors="#312651" /> // Loading indicator for the data source
+      ) : error ? (
+        <Text>Something went wrong</Text> //  Something went wrong error message
+      ) : (
+        data.map((item) => (
+          <UpcomingMatchesCard
+            item={item}
+            selectedMatch={selectedMatch}
+            handleCardPress={handleCardPress}
+            key={item} // TODO: Temp key, add key from API when needed
+          />
+        ))
+      )} */}
+      <View>
+        <Text>{dateToPassAsQueryItem}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default UpcomingMatchContent;
